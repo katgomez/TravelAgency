d5029fc92907373559bc64a5e985538b5bc07baa
Katherine Gomez <kat.gomez.z@gmail.com>
2024-02-15 13:13:26 +0100

    adding skeleton ApplicationServices

---
+++ ApplicationServices/Controllers/FlightReservationsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApplicationServices.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FlightReservationsController : ControllerBase
+    {
+        private IConfiguration _configuration;
+        public FlightReservationsController(IConfiguration configuration)
+        {
+            this._configuration = configuration;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<string>>> FlightReservations()
+        {
+            return Ok();
+        }
+
+    }
+}
---
+++ ApplicationServices/Models/FlightReservation.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace WS.DataServices.Model
+{
+    public class FlightReservation
+    {
+        [Key]
+        public int Id { get; set; }
+        [ForeignKey("Reservation")]
+        public int ReservationID { get; set; }
+        public string FlightId { get; set; } 
+        public string Airline {  get; set; }
+        public string DestinationAirport {  get; set; }
+        public DateOnly DestinationDate { get; set; }
+        public string DpartureAirport { get; set; }
+        public DateOnly DepartureDate { get; set; }
+        public decimal Price { get; set; }
+    }
+}
