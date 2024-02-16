ceced3b232d90a49f6f58fcb34adab24bca556ef
danielorviz <danielorviz@gmail.com>
2024-02-16 16:09:53 +0100

    Busqueda de vuelos

---
+++ ApplicationServices/Models/Flights/AirportResultDto.cs
@@ -0,0 +1,9 @@
+﻿namespace ApplicationServices.Models.Flights
+{
+    public class AirportResultDto
+    {
+        public int numberResults { get; set; }
+        public List<AirportDto> airports { get; set; }
+
+    }
+}
