7cd341f53222a926203444c4e43e55f44928e72e
Katherine Gomez <kat.gomez.z@gmail.com>
2024-02-18 22:18:35 +0100

    adding Flight Reservation

--- DataServices/Model/FlightReservation.cs
+++ DataServices/Model/FlightReservation.cs
@@ -9,12 +9,15 @@ namespace DataServices.Model
         public int Id { get; set; }
         [ForeignKey("Reservation")]
         public int ReservationID { get; set; }
+        public int CodeOfItinerary { get; set; }
         public string FlightId { get; set; }
         public string Airline { get; set; }
-        public string DestinationAirport { get; set; }
-        public DateTime DestinationDate { get; set; }
+        public string ArrivalAirport { get; set; }
+        public DateTime ArrivalDate { get; set; }
         public string DepartureAirport { get; set; }
         public DateTime DepartureDate { get; set; }
+        public int numberOfStops { get; set; }
+        public string Duration { get; set; }
         public decimal Price { get; set; }
     }
 }
