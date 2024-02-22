302fd89c9dcc68d3a47e69deb66aaf6acfdfb6ea
Katherine Gomez <kat.gomez.z@gmail.com>
2024-02-22 19:49:43 +0100

    copy and paste file

--- WebService/entrypoint.sh
+++ WebService/entrypoint.sh
@@ -1,6 +1,5 @@
-#!/bin/bash
+#!/bin/bash -e
 
-#Check for the ENVIRONMENT variable and replace the file based on that
 if [[ $ENVIRONMENT = "Dev" ]]; then
    cp /usr/share/nginx/html/config/app-config.dev.json /usr/share/nginx/html/app-config.json
 fi
